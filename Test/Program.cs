bdd07342ef4b31194eecc95b49f53cdc4c05d9ca
Noormees <garry.lobanoff@gmail.com>
2023-02-26 19:39:35 +0200

    add week 8 code

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+string path = @"G:\Мой диск\TKTK\3 aasta\Programmeerimise alused\data\";
+string fileName = "chuck.txt";
+//string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+//Console.WriteLine(lines.Length);
+
+string[] lines = File.ReadAllLines(path + fileName);
+
+foreach (string hero in lines)
+{
+    Console.WriteLine(hero);
+}
---
+++ Week_EpicGame/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//andmed
+//string[] heroes = {"Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo", "Superman", "Spiderman"};
+//string[] villains = {"Voldemort", "Darth Vader", "Sauron", "Joker", "Dracula" };
+
+
+string folderPath = @"G:\Мой диск\TKTK\3 aasta\Programmeerimise alused\data\";
+string heroFail = "heroes.txt";
+string villainsFail = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFail));//voib kirjutada "folderPath+heroFail"
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainsFail));
+
+
+string[] weapon = { "Magic Wand", "Banana", "Wooden Sword", "Siper Rower", "Knife",}; 
+
+
+string hero = GetRandomValueFromArray(heroes); // heroes - massiv; [] - funktsioon
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string vallainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} with {vallainWeapon} tries to take over the World!");
+
+
+static string GetRandomValueFromArray(string[] someArray) // kutsume funk GetRandomValueFromArray; someArray - suvaline vaartus, ehk massiv string tyypi
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;   
+}
